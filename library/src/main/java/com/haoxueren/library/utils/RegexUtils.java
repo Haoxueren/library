24196f70dd159d513e472fb74e848d272f0a78aa
haoxueren <haoxueren@126.com>
2020-02-25 14:24:54 +0800

    正则表达式工具类RegexUtils

---
+++ library/src/main/java/com/haoxueren/library/utils/RegexUtils.java
@@ -0,0 +1,16 @@
+package com.haoxueren.library.utils;
+
+public class RegexUtils {
+
+    private RegexUtils() {
+    }
+
+    /**
+     * 判断是否为URL
+     */
+    public static boolean isUrl(String url) {
+        // 正则表达式来自StackOverflow
+        return url.matches("(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]");
+    }
+
+}
