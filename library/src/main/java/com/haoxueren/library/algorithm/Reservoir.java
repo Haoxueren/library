7b7589e0475001d410165a2774a5044361b48c40
haoxueren <haoxueren@126.com>
2020-02-25 11:08:27 +0800

    添加分布式蓄水池抽样算法

--- library/src/main/java/com/haoxueren/library/algorithm/Reservoir.java
+++ library/src/main/java/com/haoxueren/library/algorithm/Reservoir.java
@@ -1,5 +1,6 @@
 package com.haoxueren.library.algorithm;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
 
@@ -29,5 +30,37 @@ public class Reservoir {
                 sample[keep] = element;
             }
         }
+    }
+
+    /**
+     * 分布式蓄水池抽样算法
+     * <p>
+     * 假设有k台机器，每台机器分别进行单机版蓄水池抽样，分别抽样完毕后得到两个长度为k的数组：
+     * 一个是每台机器接收的数据总量N(i)，一个是每台机器的抽样样本数组A(i)
+     */
+    public static void parallelSampling(int[] N, String[][] A) {
+        int k = N.length; // 机器数量
+        int sumN = 0; // 蓄水池的样本总量
+        for (int n : N) {
+            sumN += n;
+        }
+        // 对样本数据进行二次抽样
+        String[] simples = new String[k]; // 用来保存最终采样结果
+        Random random = new Random();
+        int indexK = random.nextInt(sumN); // 产生一个随机数k，用于抽样一台机器
+        for (int time = 0; time < k; time++) { // 进行k次再抽样
+            int sumK = 0;
+            for (int i = 0; i < k; i++) { // 计算k属于哪个区间
+                sumK += N[i];
+                if (indexK < sumK) {
+                    String[] target = A[i];
+                    int r = random.nextInt(target.length);
+                    simples[time] = target[r];
+                    return;
+                }
+            }
+        }
+        // 打印最终抽样结果
+        System.out.println(Arrays.toString(simples));
     }
 }
