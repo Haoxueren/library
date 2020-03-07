999cc722fb50b21264244a05d84b2f20f0c53f8d
haoxueren <haoxueren@126.com>
2020-03-07 16:16:00 +0800

    添加FilterEditText

---
+++ library/src/main/java/com/haoxueren/library/java8/Action.java
@@ -0,0 +1,11 @@
+package com.haoxueren.library.java8;
+
+/**
+ * A functional interface similar to Runnable
+ */
+public interface Action {
+    /**
+     * Runs the action
+     */
+    void run() ;
+}
--- library/src/main/java/com/haoxueren/library/view/FilterEditText.java
+++ library/src/main/java/com/haoxueren/library/view/FilterEditText.java
@@ -1,6 +1,7 @@
 package com.haoxueren.library.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.InputFilter;
 import android.text.InputType;
@@ -8,7 +9,10 @@ import android.text.Spanned;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 
+import com.haoxueren.library.R;
 import com.haoxueren.library.log.MyLog;
+
+import java.util.Arrays;
 
 /**
  * 用户新输入的内容必须满足指定的正则表达式
@@ -16,7 +20,7 @@ import com.haoxueren.library.log.MyLog;
  */
 public class FilterEditText extends AppCompatEditText implements InputFilter {
 
-    private String regex = "[0-9-:\\s]*";
+    private AttributeHolder holder;
 
     public FilterEditText(Context context) {
         super(context);
@@ -24,15 +28,34 @@ public class FilterEditText extends AppCompatEditText implements InputFilter {
 
     public FilterEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.setFilters(new InputFilter[]{this});
+        holder = new AttributeHolder(context, attrs);
+        InputFilter[] filters = this.getFilters();
+        InputFilter[] newFilters = new InputFilter[filters.length + 1];
+        System.arraycopy(filters, 0, newFilters, 0, filters.length);
+        newFilters[filters.length] = this; // 追加当前Filter
+        this.setFilters(newFilters);
     }
 
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int destStart, int destEnd) {
-        String input = source.toString();
-        if (input.matches(regex)) {
-            return null;
+        if (TextUtils.isEmpty(holder.regex)) {
+            return null; // 无限制，不处理
         }
-        return "";
+        String input = source.toString();
+        if (input.matches(holder.regex)) {
+            return null; // 满足条件，放行
+        }
+        return ""; // 不满足条件，返回空串
+    }
+
+    class AttributeHolder {
+
+        String regex;
+
+        AttributeHolder(Context context, AttributeSet attrs) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.FilterEditText);
+            regex = typedArray.getString(R.styleable.FilterEditText_filterRegex);
+            typedArray.recycle();
+        }
     }
 }
